0711ab64c8b9cec82bc9be0ac946ed8ff670c1c7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-18 18:56:13 +0300

    react using CDNs

---
+++ react info site/README.md
@@ -0,0 +1,1 @@
+![Alt text](react-info-site.png)
---
+++ react info site/script.js
@@ -0,0 +1,14 @@
+const navbar = (
+    <nav>
+        <h1 className="header">JSX</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
+
+
