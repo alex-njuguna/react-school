e91bbe7a1eb950620ab3f366c05e569734ce8750
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-20 19:50:58 +0300

    last nails on intro

---
+++ intro/intro/src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+// import ReactDOM from "react-dom"
+
+
+function App(){
+  return (
+     <div>
+        <h1 className="header">My awesome website in React</h1>
+        <h3>Reasons I love React:</h3>
+        <ol className="list-group">
+            <li className="list-group-item">It's composable</li>
+            <li className="list-group-item">It's declarative</li>
+            <li className="list-group-item">It's a hireable skill</li>
+            <li className="list-group-item">It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+  )
+}
+   
+
+export default App
