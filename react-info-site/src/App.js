9733c892a6f66cebe8361068fa4bd7a8102e1125
alex-njuguna <njugunakinuthia013@gmail.com>
2023-11-20 21:50:25 +0300

    building from scratch part 1

--- react-info-site/src/App.js
+++ react-info-site/src/App.js
@@ -1,40 +1,20 @@
-// import logo from './logo.svg';
-import React from "react"
-import './App.css';
-import Header from "./Header"
+import React from "react";
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
 
 
-function Footer(){
-  return (
-    <footer className="page-footer">
-        &copy; 2023 alex njuguna. All rights reserved  
-    </footer>
-  )
-}
-
-function MainContent(){
-  return (
-    <div className="main-content">
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>It's a popular library</li>
-        <li>Improve my tech stack</li>
-        <li>It's a hireble skill</li>
-      </ol>
-    </div>
-  )
+function App(){
+    return (
+        <div>
+            <Navbar />
+            <Main />
+        </div>
+    )
 }
 
 
-function App() {
-  return(
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  )
-    
-}
+export default App
 
-export default App;
+
+
+
---
+++ react-info-site/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+export default function Navbar(){
+    return (
+        <h1>Navbar goes here</h1>
+    )
+}
