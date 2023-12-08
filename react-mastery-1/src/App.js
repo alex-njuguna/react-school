6f90332bcecbce16998927daa50ae5e2cc313d7f
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-08 23:56:41 +0300

    adds App.css

--- react-mastery-1/src/App.js
+++ react-mastery-1/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./containers"
 import { Brand, CTA, Navbar } from "./components"
+import "./App.css"
 
 const App = () => {
   return (
