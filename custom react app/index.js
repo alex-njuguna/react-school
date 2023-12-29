01a2c5f1184de667336c48327357e88404c8fc2a
alex-njuguna <njugunakinuthia013@gmail.com>
2023-12-29 23:33:28 +0300

    create a custom react element and field where to deploy it

---
+++ custom react app/index.js
@@ -0,0 +1,9 @@
+// build a custom react element
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+} 
