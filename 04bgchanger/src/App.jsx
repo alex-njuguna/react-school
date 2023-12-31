b08013bfc7b698cb7d62e53a8c927bba7cf3fb07
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-01 00:45:03 +0300

    adds more styling

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -11,10 +11,10 @@ function App() {
   return (
     <div className="container-fluid d-flex flex-column min-vh-100" style={{backgroundColor: color}}>
       <div className="container-md text-center fixed-bottom">
-        <div className="btn-group p-3">
-          <button onClick={() => setColor('#007BFF')} className='btn btn-lg btn-primary rounded m-3'>Primary</button>
-          <button onClick={() => setColor('#28A745')} className='btn btn-lg btn-success rounded m-3'>Success</button>
-          <button onClick={() => setColor('#DC3545')} className='btn btn-lg btn-danger rounded m-3'>Danger</button>
+        <div className="btn-group p-1 bg-dark m-2 shadow">
+          <button onClick={() => setColor('#007BFF')} className='btn btn-lg btn-outline-primary rounded m-3'>Primary</button>
+          <button onClick={() => setColor('#28A745')} className='btn btn-lg btn-outline-success rounded m-3'>Success</button>
+          <button onClick={() => setColor('#DC3545')} className='btn btn-lg btn-outline-danger rounded m-3'>Danger</button>
         </div>
         
       </div>
