0bc5238fc5002d3b39a28c1eb01ffdfb26ba3744
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-01 00:40:08 +0300

    optimize code

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -4,17 +4,17 @@ function App() {
 
   const [color, setColor] = useState('olive')
 
-  const changeColor = (color) => {
-    setColor(color)
-  }
+  // const changeColor = (color) => {
+  //   setColor(color)
+  // }
 
   return (
     <div className="container-fluid d-flex flex-column min-vh-100" style={{backgroundColor: color}}>
       <div className="container-md text-center fixed-bottom">
         <div className="btn-group p-3">
-          <button onClick={() => changeColor('#007BFF')} className='btn btn-lg btn-primary rounded m-3'>Primary</button>
-          <button onClick={() => changeColor('#28A745')} className='btn btn-lg btn-success rounded m-3'>Success</button>
-          <button onClick={() => changeColor('#DC3545')} className='btn btn-lg btn-danger rounded m-3'>Danger</button>
+          <button onClick={() => setColor('#007BFF')} className='btn btn-lg btn-primary rounded m-3'>Primary</button>
+          <button onClick={() => setColor('#28A745')} className='btn btn-lg btn-success rounded m-3'>Success</button>
+          <button onClick={() => setColor('#DC3545')} className='btn btn-lg btn-danger rounded m-3'>Danger</button>
         </div>
         
       </div>
