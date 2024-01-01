fe7173c3ad6d2d5fdf806a3f4673a2df8c1a1b93
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-01 03:42:12 +0300

    interface setup

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,34 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  /*states for variables
+  length - set the length of or password
+  numberAllowed - to set if the password should have numbers
+  charAllowed - set if the password should have characters
+  password - set password from the aforementioned states
+  */
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container d-flex flex-column min-vh-100 align-items-center text-center text-light p-4">
+        <h4 className="fw-bold">Password Generator</h4>
+        <div className="container m-3">
+          <input 
+          className='rounded border-0 w-75 p-2'
+          type="text" 
+          value={password}
+          placeholder='Password'
+          readOnly
+          />
+          &nbsp;&nbsp;
+          <button className='btn  btn-primary rounded'>copy</button>
+        </div>
+        
+        <div className="container m-3">
+          <input 
+          type="range"
+          min={6}
+          max={20}
+          value={length} 
+          onChange={(e) => setLength(e.target.value)}
+          name="" 
+          id="" 
+          />&nbsp;&nbsp;
+          <label htmlFor="length">Length: {length}</label>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+
+          <input
+          type='checkbox'
+          defaultChecked={numberAllowed}
+          onChange={() => {setNumberAllowed((prev) => !prev)}}
+          />&nbsp;
+          <label htmlFor="numbers">Numbers</label>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+
+          <input
+          type='checkbox'
+          defaultChecked={charAllowed}
+          onChange={() => {setCharAllowed((prev) => !prev)}}
+          />&nbsp;
+          <label htmlFor="Character">Char</label>
+        </div>
+
+      
+    </div>
   )
 }
 
