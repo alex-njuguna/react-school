ea9d4d95a7c5582a5839fae058103f79febb90f3
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-04 02:19:46 +0300

    adds a convert currency function

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 import InputBox from './components/InputBox'
 
 function App() {
+
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('kes')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
 
   return (
    <div className="container d-flex flex-column min-vh-100">
