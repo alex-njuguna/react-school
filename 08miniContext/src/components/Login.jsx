987b6b8aec8106a200c2fae5c594b3378309fc05
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-08 10:34:46 +0300

    create user context

---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    // handle click event
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input 
+      type="text"
+      placeholder='username'
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      />
+      <br />
+      <input 
+      type="password" 
+      placeholder='password'
+      value={password}
+      onChange={(e) => setPassword(e.target.value)} 
+      />
+
+      <button onClick={handleSubmit}>Login</button>
+    </div>
+  )
+}
+
+export default Login
---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// create context
+const UserContext = React.createContext()
+
+
+export default UserContext
