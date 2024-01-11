5056711a8363f3436c032e0adbdc02647e36ab32
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-12 01:52:54 +0300

    implementing currying and piping

--- intro-to-redux/src/App.jsx
+++ intro-to-redux/src/App.jsx
@@ -3,22 +3,31 @@ import { compose, pipe } from 'lodash/fp'
 let input = '   JavaScript   '
 // let output = `<div>${input.trim()}</div>`
 
-const trim = (str) => str.trim()
-const wrapInDiv = (str) => `<div>${str}</div>`
-const toLowerCase = (str) => str.toLowerCase()
+// const trim = (str) => str.trim()
+// const wrapInDiv = (str) => `<div>${str}</div>`
+// const toLowerCase = (str) => str.toLowerCase()
 
 // const result = wrapInDiv(toLowerCase(trim(input)))
 
 // passing function in referece using compose
-const transform = compose(wrapInDiv, toLowerCase, trim)
-transform(input)
+// const transform = compose(wrapInDiv, toLowerCase, trim)
+// transform(input)
 
 // passing function in referece using pipe
-const transform2 = pipe(trim, toLowerCase, wrapInDiv)
-transform2(input)
+// const transform2 = pipe(trim, toLowerCase, wrapInDiv)
+// transform2(input)
 
 
 /*
 Pipe unlike compose allows for referencing functions by the order which they will operate from left to right.
 Compose the order is right to left 
 */
+
+
+// using currying with pipe
+const trim = (str) => str.trim()
+const wrap = (type, str) => `<${type}>${str}</${type}>`
+const toLowerCase = (str) => str.toLowerCase()
+
+const transform = pipe(trim, toLowerCase, wrap('span'))
+transform(input)
