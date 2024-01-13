856e9afa23d47d9ad615fe5d675964aa9736b800
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-13 22:22:21 +0300

    explanantion of the code in app.js

--- intro-to-redux-2/src/App.jsx
+++ intro-to-redux-2/src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
 import { reducer, store } from './components/index'
 
+
+/**
+ * logging store shows the various functionalities to operate the store
+ * getState() - shows the current state of the store, i.e. the data in the store
+ * dispatch() - initiates an action already defined in the reducer
+ */
 function App() {
 
   store.dispatch ({
