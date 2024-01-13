f56ca14616a4b8eb9435d3de0438a6cca6d726a5
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-13 21:27:18 +0300

    developing a reducer based on the actions of add or remove using the switch statement

--- intro-to-redux-2/src/components/reducer.js
+++ intro-to-redux-2/src/components/reducer.js
@@ -24,3 +24,24 @@ function reducer(state = [], action) {
     }
     return state
 }
+
+
+// implementing the above with switch statement
+function reducer2(state, action) {
+    switch (action.type) {
+        case 'bugAdded':
+            return [
+                ...state, {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ]
+            
+        case 'bugRemoved':
+            return state.filter((bug) => bug.id !== action.payload.id)
+
+        default:
+            return state
+    }
+}
