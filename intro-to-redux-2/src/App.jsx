c2c00f6d456b99da6203f7bfd584258d6a2c13a7
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-13 22:35:02 +0300

    use subscribe and unsubscribe method to allow for only tracking in specific areas

--- intro-to-redux-2/src/App.jsx
+++ intro-to-redux-2/src/App.jsx
@@ -6,8 +6,13 @@ import { reducer, store } from './components/index'
  * logging store shows the various functionalities to operate the store
  * getState() - shows the current state of the store, i.e. the data in the store
  * dispatch() - initiates an action already defined in the reducer
+ * subscribe() - displays changes happening in the store, to prevent memory leaks when the user changes the page where the subscribe method is not needed, it is assigned to a variable(unsubscribe) and called.  
  */
 function App() {
+
+  const unsubscribe = store.subscribe(() => {
+    console.log('store changed', store.getState())
+  })
 
   store.dispatch ({
     type: 'bugAdded', 
@@ -16,13 +21,14 @@ function App() {
     }
   })
 
+  unsubscribe()
+
   store.dispatch({
     type: 'bugRemoved',
     payload: {
       id: 1
     }
   })
-
 
   console.log(store.getState())
 
