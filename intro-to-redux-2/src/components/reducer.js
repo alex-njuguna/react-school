41f0f320f087e27a66d7a30349884400c50ba331
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-13 21:57:57 +0300

    create a store with createStore from redux

--- intro-to-redux-2/src/components/reducer.js
+++ intro-to-redux-2/src/components/reducer.js
@@ -10,24 +10,26 @@ let lastId = 0
 * if action type is bugRemoved, get all the data from state and filter the one whose id is passed to be removed
 * else return state as is
 */
-function reducer(state = [], action) {
-    if (action.type === 'bugAdded') {
-        return [
-            ...state, {
-                id: ++lastId,
-                description: action.payload.description,
-                resolved: false
-            }
-        ]
-    } else if (action.type === 'bugRemoved') {
-        return state.filter((bug) => bug.id !== action.payload.id)
-    }
-    return state
-}
+
+
+// function reducer(state = [], action) {
+//     if (action.type === 'bugAdded') {
+//         return [
+//             ...state, {
+//                 id: ++lastId,
+//                 description: action.payload.description,
+//                 resolved: false
+//             }
+//         ]
+//     } else if (action.type === 'bugRemoved') {
+//         return state.filter((bug) => bug.id !== action.payload.id)
+//     }
+//     return state
+// }
 
 
 // implementing the above with switch statement
-function reducer2(state = [], action) {
+export default function reducer2(state = [], action) {
     switch (action.type) {
         case 'bugAdded':
             return [
