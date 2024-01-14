74e511babc4209e80c3d64fb578f098a18d440ce
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-14 03:32:30 +0300

    starting with react reduc toolkit

---
+++ 0reduxtoolkit/src/app/store.js
@@ -0,0 +1,12 @@
+/**
+ * use configureStore to setup a store
+ * export the store
+ */
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {
+        
+    }
+})
