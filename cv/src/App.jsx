58e7f63cc1d6350c985de19654cc4a471182ae04
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-29 02:00:45 +0300

    personal infornation component

---
+++ cv/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import PersonalInfo from "./components/PersonalInfo";
+
+export default function App() {
+  return (
+    <>
+      <PersonalInfo />
+    </>
+  );
+}
---
+++ cv/src/components/PersonalInfo.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./PersonalInfo.css";
+
+export default function PersonalInfo() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  return (
+    <>
+      <h2>Personal Information</h2>
+      <div>
+        <fieldset>
+          <div className="field">
+            <label htmlFor="name">Names: </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="phone">Phone: </label>
+            <input
+              type="tel"
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+        </fieldset>
+      </div>
+    </>
+  );
+}
